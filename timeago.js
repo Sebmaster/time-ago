4c4646753b88957ceb64b38fe8b62d24b04c010b
Sebastian Mayr <sebmaster16@gmail.com>
2013-07-22 21:40:24 +0200

    Don't pollute the global namespace

--- timeago.js
+++ timeago.js
@@ -1,14 +1,14 @@
-module.exports = function(){
-    
-    var second = 1000, 
-        minute = second*60, 
-        hour = minute*60, 
-        day = hour*24, 
-        week = day*7, 
-        month = day*30, 
-        year = day*365;
+var second = 1000, 
+    minute = second*60, 
+    hour = minute*60, 
+    day = hour*24, 
+    week = day*7, 
+    month = day*30, 
+    year = day * 365;
 
-    global._timeago = function(nd){
+module.exports = {
+
+    timeago: function(nd){
         var r = Math.round, pl = function(v, n){ return n + ' ' + v + (n>1 ? 's':'') + ' ago'};
         var ts = new Date().getTime() - new Date(nd).getTime();
         if(ts < second) return ts + ' ms ago';
@@ -19,16 +19,16 @@ module.exports = function(){
         if(ts > week && ts < month) return pl('week', r(ts/week));
         if(ts > month && ts < year) return pl('month', r(ts/month));
         return pl('year', r(ts/year));
-    }
+    },
     
-    global._today = function(){
+    today: function(){
         var now = new Date();
         var Weekday = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
         var Month = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
         return Weekday[now.getDay()]+", "+Month[now.getMonth()]+" "+now.getDate()+", "+now.getFullYear();
-    }
+    },
 
-    global._timefriendly = function(s){
+    timefriendly: function(s){
         var t = s.match(/(\d).([a-z]*?)s?$/);
         return t[1] * eval(t[2]);
     }
